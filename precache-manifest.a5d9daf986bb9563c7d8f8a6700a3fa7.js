daa0fe3c1c20e83eb4cb2e77aae864d77563e62e
Mulia Arifandi Nasution <mul14.net@gmail.com>
2020-03-05 09:59:22 +0700

    Initial commit

---
+++ precache-manifest.a5d9daf986bb9563c7d8f8a6700a3fa7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60645b98c5ecc35adfd6",
+    "url": "/css/app.5a8f0e5c.css"
+  },
+  {
+    "revision": "9cdda90a4bace0a9774ea13ab542acf9",
+    "url": "/employee.csv"
+  },
+  {
+    "revision": "4eb862b5b9067bae320433dddd9376c3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4d76b96f771644b03cb3",
+    "url": "/js/about.31e2400b.js"
+  },
+  {
+    "revision": "60645b98c5ecc35adfd6",
+    "url": "/js/app.326a68b3.js"
+  },
+  {
+    "revision": "3b167a59b27af0fd6424",
+    "url": "/js/chunk-vendors.5cc5cf21.js"
+  },
+  {
+    "revision": "5bc475b192601396a191",
+    "url": "/js/home.f1c858ba.js"
+  },
+  {
+    "revision": "9aacc6b5a91f81f9ad7ca8b2d3de2d88",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
---
+++ precache-manifest.e512848ac75248b366024bf595be5a1d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e37cb49a0ac785276fa1",
+    "url": "/css/app.5a8f0e5c.css"
+  },
+  {
+    "revision": "bea0b0dbbdc777d4305e88c393a4e89a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5b771e95064c062b0ed7",
+    "url": "/js/about-legacy.4afac541.js"
+  },
+  {
+    "revision": "e37cb49a0ac785276fa1",
+    "url": "/js/app-legacy.1eba8974.js"
+  },
+  {
+    "revision": "a3cf270634ce2a791bfb",
+    "url": "/js/chunk-vendors-legacy.4639532f.js"
+  },
+  {
+    "revision": "34c4e84689318f08f946",
+    "url": "/js/home-legacy.489d3467.js"
+  },
+  {
+    "revision": "9aacc6b5a91f81f9ad7ca8b2d3de2d88",
+    "url": "/manifest.json"
+  }
+]);
